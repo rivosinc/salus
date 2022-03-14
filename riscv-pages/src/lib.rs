aef75834c479455e70ea73d9106c828ef38c7a0f
Dylan Reid <dgreid@rivosinc.com>
2022-03-14 12:42:06 -0700

    riscv-pages: Add unit tests
    Adding tests for iterators to make sure bounds checks are correct.
    
    Signed-off-by: Dylan Reid <dgreid@rivosinc.com>

--- riscv-pages/src/lib.rs
+++ riscv-pages/src/lib.rs
@@ -7,6 +7,11 @@
 //! - `Page` is the basic building block, representing pages of host supervisor memory.
 #![no_std]
 
+// For testing use the std crate.
+#[cfg(test)]
+#[macro_use]
+extern crate std;
+
 mod page;
 mod page_owner_id;
 mod sequential_pages;

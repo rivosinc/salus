fc7734f9f026328a99d99a33b13dbeee4f4c2b79
Dylan Reid <dgreid@rivosinc.com>
2022-03-14 00:18:48 -0700

    page-collections: Add crate-level comment
    The purpose of these types needed more explanation.
    
    Signed-off-by: Dylan Reid <dgreid@rivosinc.com>

--- page-collections/src/lib.rs
+++ page-collections/src/lib.rs
@@ -17,6 +17,18 @@
 //! Each page is given to the hypervisor for a specific purpose. For example, a page can be used to
 //! hold a guest's state(`PageBox<State>`), or a list of children VMs (`PageVec<Guest>).
 
+//! Type wrappers that are backed by whole pages.
+//!
+//! When working without an allocator, but with a set of available whole pages, it is useful to to
+//! store data in those pages and track them as somewhat-normal rust types.
+//!
+//! Providing types that closely mirror standard rust containers but are backed by these loaned
+//! pages makes the hypervisor code easier to comprehend when coming from a more standard rust
+//! codebase.
+//!
+//! Each page is given to the hypervisor for a specific purpose. For example, a page can be used to
+//! hold a guest's state(`PageBox<State>`), or a list of children VMs (`PageVec<Guest>).
+
 extern crate alloc;
 
 pub mod page_box;

4bcbd786db0baebd7ea972a597fb694a628140d2
Atul Khare <atulkhare@rivosinc.com>
2022-05-24 14:46:26 -0700

    Use GetSelfMeasurement call in guestvm
    The change exercises the new functionality from the guestvm.

--- src/guestvm.rs
+++ src/guestvm.rs
@@ -13,8 +13,10 @@ extern crate alloc;
 mod asm;
 mod ecall;
 mod print_sbi;
+
 use ecall::ecall_send;
 use print_sbi::*;
+use sbi::SbiMessage;
 
 // Dummy global allocator - panic if anything tries to do an allocation.
 struct GeneralGlobalAlloc;
@@ -40,14 +42,37 @@ pub fn alloc_error(_layout: Layout) -> ! {
 #[no_mangle]
 #[allow(clippy::zero_ptr)]
 extern "C" fn kernel_init() {
+    const USABLE_RAM_START_ADDRESS: u64 = 0x8020_0000;
+    const NUM_GUEST_DATA_PAGES: u64 = 10;
+    const PAGE_SIZE_4K: u64 = 4096;
     // Safe because this is the very begining of the program and nothing has set the console driver
     // yet.
     unsafe {
         SbiConsoleDriver::new().use_as_console();
     }
 
+    let measurement_page_addr = USABLE_RAM_START_ADDRESS + NUM_GUEST_DATA_PAGES * PAGE_SIZE_4K;
+    let msg = SbiMessage::Measurement(sbi::MeasurementFunction::GetSelfMeasurement {
+        measurement_version: 1,
+        measurement_type: 1,
+        page_addr: measurement_page_addr,
+    });
+
     println!("*****************************************");
     println!("Hello world from Tellus guest            ");
+
+    match ecall_send(&msg) {
+        Err(e) => {
+            println!("Guest measurement error {e:?}");
+            panic!("Guest measurement call failed");
+        }
+        Ok(_) => {
+            let measurement =
+                unsafe { core::ptr::read_volatile(measurement_page_addr as *const u64) };
+            println!("Guest measurement was {measurement:x}");
+        }
+    }
+
     println!("Exiting guest by causing a fault         ");
     println!("*****************************************");
 

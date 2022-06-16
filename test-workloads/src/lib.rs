1d6803be84eece3229ee0379a5f6e797abe3886a
Dylan Reid <dgreid@rivosinc.com>
2022-06-16 11:19:42 -0700

    test-workloads: Add doc comments
    Add doc comments so a later commit can enable a check for docs on
    all public interfaces to CI.
    
    Signed-off-by: Dylan Reid <dgreid@rivosinc.com>

--- test-workloads/src/lib.rs
+++ test-workloads/src/lib.rs
@@ -1,3 +1,10 @@
+// Copyright (c) 2022 by Rivos Inc.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+//! This crate provides test workloads to exercise the salus hypervisor and the Confidential
+//! computing API.
+
 #![no_std]
 
 use s_mode_utils::abort::abort;
@@ -5,6 +12,7 @@ use s_mode_utils::print_sbi::*;
 
 mod asm;
 
+/// Panics the running test workload.
 #[panic_handler]
 pub fn panic(info: &core::panic::PanicInfo) -> ! {
     println!("panic : {:?}", info);

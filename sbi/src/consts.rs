51e4347f9ae9c553075034a594df1eb8e3b397b9
Dylan Reid <dgreid@rivosinc.com>
2022-06-28 00:01:06 -0700

    sbi: Update TEE extension ID
    The TEE extension ID has changed to "ATEE".
    
    Signed-off-by: Dylan Reid <dgreid@rivosinc.com>

--- sbi/src/consts.rs
+++ sbi/src/consts.rs
@@ -9,7 +9,7 @@ pub const EXT_PUT_CHAR: u64 = 0x01;
 pub const EXT_BASE: u64 = 0x10;
 pub const EXT_HART_STATE: u64 = 0x48534D;
 pub const EXT_RESET: u64 = 0x53525354;
-pub const EXT_TEE: u64 = 0x544545;
+pub const EXT_TEE: u64 = 0x41544545;
 pub const EXT_MEASUREMENT: u64 = 0x5464545;
 
 pub const SBI_SUCCESS: i64 = 0;

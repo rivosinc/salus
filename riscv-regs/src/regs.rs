e1a9b271eae1cf7d204ba826e7d5299e7c8cb71c
Andrew Bresticker <abrestic@rivosinc.com>
2022-07-06 11:24:30 -0400

    riscv-regs: Add helper to convert raw register index to GprIndex
    So we can turn the register index in an instruction into a usable
    GprIndex. Also add Zero (X0) to the enum for completeness.
    
    Signed-off-by: Andrew Bresticker <abrestic@rivosinc.com>

--- riscv-regs/src/regs.rs
+++ riscv-regs/src/regs.rs
@@ -12,8 +12,11 @@
 pub struct GeneralPurposeRegisters([u64; 32]);
 
 /// Index of risc-v general purpose registers in `GeneralPurposeRegisters`.
+#[repr(u32)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum GprIndex {
-    RA = 0,
+    Zero = 0,
+    RA,
     GP,
     TP,
     S0,
@@ -44,6 +47,50 @@ pub enum GprIndex {
     T5,
     T6,
     SP,
+}
+
+impl GprIndex {
+    pub fn from_raw(raw: u32) -> Option<Self> {
+        use GprIndex::*;
+        let index = match raw {
+            0 => Zero,
+            1 => RA,
+            2 => SP,
+            3 => GP,
+            4 => TP,
+            5 => T0,
+            6 => T1,
+            7 => T2,
+            8 => S0,
+            9 => S1,
+            10 => A0,
+            11 => A1,
+            12 => A2,
+            13 => A3,
+            14 => A4,
+            15 => A5,
+            16 => A6,
+            17 => A7,
+            18 => S2,
+            19 => S3,
+            20 => S4,
+            21 => S5,
+            22 => S6,
+            23 => S7,
+            24 => S8,
+            25 => S9,
+            26 => S10,
+            27 => S11,
+            28 => T3,
+            29 => T4,
+            30 => T5,
+            31 => T6,
+            _ => {
+                return None;
+            }
+        };
+        Some(index)
+    }
 }
 
 impl GeneralPurposeRegisters {

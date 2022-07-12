9ed6a7b27ded5187385d5332f4c6fcf79a00d458
Samuel Ortiz <sameo@rivosinc.com>
2022-07-12 15:39:37 +0200

    attestation: Add the TCG DICE extension
    This X.509 extension is defined in the TCG Attestation architecture
    specification.
    
    It allows for carrying a TCB evidence as a certificate custom extension.
    
    Signed-off-by: Samuel Ortiz <sameo@rivosinc.com>

---
+++ attestation/src/extensions/dice.rs
@@ -0,0 +1,6 @@
+// Copyright (c) 2022 by Rivos Inc.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+/// DICE TcbInfo extension
+pub mod tcbinfo;

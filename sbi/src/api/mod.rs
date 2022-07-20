5c85942b3d2925bb8e366caa465959acdf908782
Dylan Reid <dgreid@rivosinc.com>
2022-07-20 16:19:21 -0700

    sbi: Add api for hart state
    Brings HartState closer to TSM's model of providing an api instead of
    forcing users to create messages and do an ecall manually.
    
    Currently only hart_start is called so that's all that's provided by the
    api for now.
    
    Signed-off-by: Dylan Reid <dgreid@rivosinc.com>

--- sbi/src/api/mod.rs
+++ sbi/src/api/mod.rs
@@ -2,6 +2,10 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+/// Host interfaces for hart state management.
+#[cfg(all(target_arch = "riscv64", target_os = "none"))]
+pub mod state;
+
 /// Host interfaces for confidential computing.
 #[cfg(all(target_arch = "riscv64", target_os = "none"))]
 pub mod tsm;

b5fc71959f76973e5f5600e8710e04841deeafad
Andrew Bresticker <abrestic@rivosinc.com>
2022-08-10 08:53:43 -0400

    iommu: Add support for building MSI page tables
    MSI page tables are a flat (single-level) paging structure used to provide
    translation for MSI writes. The input to an MSI page table is the guest
    physical address of an MSI write (i.e. the address of an virtualized IMSIC
    in guest physical address space) and the output is the supervisor physical
    address of an IMSIC interrupt file (or a memory-resident interrupt file,
    which isn't yet supported). The recently-introduced ImsicGeometry and
    ImsicLocation to specify mappings in the MSI page table.
    
    Signed-off-by: Andrew Bresticker <abrestic@rivosinc.com>

--- drivers/src/iommu/error.rs
+++ drivers/src/iommu/error.rs
@@ -2,6 +2,9 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+use riscv_pages::SupervisorPageAddr;
+
+use crate::imsic::ImsicLocation;
 use crate::pci::PciError;
 
 /// Errors resulting from interacting with the IOMMU.
@@ -19,6 +22,20 @@ pub enum Error {
     MissingGStageSupport,
     /// Missing required MSI translation support.
     MissingMsiSupport,
+    /// Not enough pages were supplied to create an MSI page table.
+    InsufficientMsiTablePages,
+    /// The supplied MSI page table pages were not properly aligned.
+    MisalignedMsiTablePages,
+    /// Ownership mismatch in MSI page table pages.
+    UnownedMsiTablePages,
+    /// Attempt to map an invalid IMSIC location in an MSI page table.
+    InvalidImsicLocation(ImsicLocation),
+    /// The destination of an MSI page table mapping is not owned by the VM.
+    MsiPageNotOwned(SupervisorPageAddr),
+    /// The MSI page table entry is already mapped.
+    MsiAlreadyMapped(ImsicLocation),
+    /// The MSI page table entry is not mapped.
+    MsiNotMapped(ImsicLocation),
 }
 
 /// Holds results for IOMMU operations.

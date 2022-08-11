566970975d2913859953f333f156f843d31f08d2
Andrew Bresticker <abrestic@rivosinc.com>
2022-08-11 10:23:12 -0400

    iommu: main.rs -> core.rs
    "main" is typically used for the entrypoint to a binary Rust crate. Use
    "core" instead for the main IOMMU module.
    
    Signed-off-by: Andrew Bresticker <abrestic@rivosinc.com>

--- drivers/src/iommu/mod.rs
+++ drivers/src/iommu/mod.rs
@@ -2,10 +2,10 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+mod core;
 mod error;
-mod main;
 mod registers;
 
+pub use self::core::Iommu;
 pub use error::Error as IommuError;
 pub use error::Result as IommuResult;
-pub use main::Iommu;

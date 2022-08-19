66f31da090563beb3ee757c9ca07af79a5cb460b
Andrew Bresticker <abrestic@rivosinc.com>
2022-08-19 14:42:07 -0400

    sbi: Remove stale references to measurement extension
    It has been superseded by the attestation extension.
    
    Signed-off-by: Andrew Bresticker <abrestic@rivosinc.com>

--- sbi/src/consts.rs
+++ sbi/src/consts.rs
@@ -11,8 +11,6 @@ pub const EXT_HART_STATE: u64 = 0x48534D;
 pub const EXT_PMU: u64 = 0x504D55;
 pub const EXT_RESET: u64 = 0x53525354;
 pub const EXT_TEE: u64 = 0x41544545;
-pub const EXT_MEASUREMENT: u64 = 0x5464545;
-// TODO Replace the measurement extension once the `GetEvidence` implementation is complete
 pub const EXT_ATTESTATION: u64 = 0x41545354; // ATST
 
 pub const SBI_SUCCESS: i64 = 0;

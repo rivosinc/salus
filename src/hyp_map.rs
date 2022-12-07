5ab13ac428506efda377965be44283f41ad05285
Gianluca Guida <gianluca@rivosinc.com>
2022-12-07 18:04:58 +0000

    HypMap: Fixes and cleanup for hypervisor mappings.
    This patch simplifies hypmap by removing legacy functions and creating
    `HypMapRegion`, a structure that can be mapped into a hypervisor
    pagetable.
    
    Also, hypervisor mapping for Hw Mem Map are fixed -- although more
    work needs to be done for the hypervisor image. Host kernel and initrd
    are mapped read-only, all others, except for hypervisor image,
    read/write.
    
    Signed-off-by: Gianluca Guida <gianluca@rivosinc.com>

--- src/hyp_map.rs
+++ src/hyp_map.rs
@@ -2,62 +2,93 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+use arrayvec::ArrayVec;
 use page_tracking::{HwMemMap, HwMemRegion, HwMemRegionType, HwReservedMemType, HypPageAlloc};
 use riscv_page_tables::{FirstStagePageTable, PteFieldBits, PteLeafPerms, Sv48};
-use riscv_pages::{InternalClean, Page, PageAddr, PageSize, RawAddr, SupervisorPhys};
+use riscv_pages::{
+    InternalClean, Page, PageAddr, PageSize, RawAddr, SupervisorPhys, SupervisorVirt,
+};
 use riscv_regs::{satp, LocalRegisterCopy, SatpHelpers};
 
-// Returns the base, size, and permission pair for the given region if that region type should be
-// mapped in the hypervisor's virtual address space.
-fn hyp_map_params(r: &HwMemRegion) -> Option<(PageAddr<SupervisorPhys>, u64, PteLeafPerms)> {
-    match r.region_type() {
-        HwMemRegionType::Available => {
-            // map available memory as rwx - unser what it'll be used for.
-            Some((r.base(), r.size(), PteLeafPerms::RWX))
-        }
-        HwMemRegionType::Reserved(HwReservedMemType::FirmwareReserved) => {
-            // No need to map regions reserved for firmware use
-            None
-        }
-        HwMemRegionType::Reserved(HwReservedMemType::HypervisorImage)
-        | HwMemRegionType::Reserved(HwReservedMemType::HostKernelImage)
-        | HwMemRegionType::Reserved(HwReservedMemType::HostInitramfsImage) => {
-            Some((r.base(), r.size(), PteLeafPerms::RWX))
-        }
-        HwMemRegionType::Reserved(HwReservedMemType::HypervisorHeap)
-        | HwMemRegionType::Reserved(HwReservedMemType::HypervisorPerCpu)
-        | HwMemRegionType::Reserved(HwReservedMemType::PageMap) => {
-            Some((r.base(), r.size(), PteLeafPerms::RW))
-        }
-        HwMemRegionType::Mmio(_) => Some((r.base(), r.size(), PteLeafPerms::RW)),
-    }
+/// Maximum number of regions that will be mapped in the hypervisor.
+const MAX_HYPMAP_REGIONS: usize = 32;
+
+/// Represents a region of the virtual address space of the hypervisor.
+pub struct HypMapRegion {
+    vaddr: PageAddr<SupervisorVirt>,
+    paddr: PageAddr<SupervisorPhys>,
+    page_count: usize,
+    pte_fields: PteFieldBits,
 }
 
-// Adds an identity mapping to the given Sv48 table for the specified address range.
-fn hyp_map_region(
-    sv48: &FirstStagePageTable<Sv48>,
-    base: PageAddr<SupervisorPhys>,
-    size: u64,
-    perms: PteLeafPerms,
-    get_pte_page: &mut dyn FnMut() -> Option<Page<InternalClean>>,
-) {
-    let region_page_count = PageSize::num_4k_pages(size);
-    // Pass through mappings, vaddr=paddr.
-    let vaddr = PageAddr::new(RawAddr::supervisor_virt(base.bits())).unwrap();
-    // Add mapping for this region to the page table
-    let mapper = sv48
-        .map_range(vaddr, PageSize::Size4k, region_page_count, get_pte_page)
-        .unwrap();
-    let pte_fields = PteFieldBits::leaf_with_perms(perms);
-    for (virt, phys) in vaddr
-        .iter_from()
-        .zip(base.iter_from())
-        .take(region_page_count as usize)
-    {
-        // Safe as we will create exactly one mapping to each page and will switch to
-        // using that mapping exclusively.
-        unsafe {
-            mapper.map_addr(virt, phys, pte_fields).unwrap();
+impl HypMapRegion {
+    /// Create an HypMapRegion from a Hw Memory Map entry.
+    pub fn from_hw_mem_region(r: &HwMemRegion) -> Option<Self> {
+        let perms = match r.region_type() {
+            HwMemRegionType::Available => {
+                // map available memory as rw - unsure what it'll be used for.
+                Some(PteLeafPerms::RW)
+            }
+            HwMemRegionType::Reserved(HwReservedMemType::FirmwareReserved) => {
+                // No need to map regions reserved for firmware use
+                None
+            }
+            HwMemRegionType::Reserved(HwReservedMemType::HypervisorImage) => {
+                Some(PteLeafPerms::RWX)
+            }
+            HwMemRegionType::Reserved(HwReservedMemType::HostKernelImage)
+            | HwMemRegionType::Reserved(HwReservedMemType::HostInitramfsImage) => {
+                Some(PteLeafPerms::R)
+            }
+            HwMemRegionType::Reserved(HwReservedMemType::HypervisorHeap)
+            | HwMemRegionType::Reserved(HwReservedMemType::HypervisorPerCpu)
+            | HwMemRegionType::Reserved(HwReservedMemType::PageMap)
+            | HwMemRegionType::Mmio(_) => Some(PteLeafPerms::RW),
+        };
+
+        if let Some(pte_perms) = perms {
+            let paddr = r.base();
+            // vaddr == paddr in mapping HW memory map.
+            // Unwrap okay. `paddr` is a page addr so it is aligned to the page.
+            let vaddr = PageAddr::new(RawAddr::supervisor_virt(r.base().bits())).unwrap();
+            let page_count = PageSize::num_4k_pages(r.size()) as usize;
+            let pte_fields = PteFieldBits::leaf_with_perms(pte_perms);
+            Some(Self {
+                vaddr,
+                paddr,
+                page_count,
+                pte_fields,
+            })
+        } else {
+            None
+        }
+    }
+
+    /// Map this region into a page table.
+    fn map(
+        &self,
+        sv48: &FirstStagePageTable<Sv48>,
+        get_pte_page: &mut dyn FnMut() -> Option<Page<InternalClean>>,
+    ) {
+        let mapper = sv48
+            .map_range(
+                self.vaddr,
+                PageSize::Size4k,
+                self.page_count as u64,
+                get_pte_page,
+            )
+            .unwrap();
+        for (virt, phys) in self
+            .vaddr
+            .iter_from()
+            .zip(self.paddr.iter_from())
+            .take(self.page_count)
+        {
+            // Safe as we will create exactly one mapping to each page and will switch to
+            // using that mapping exclusively.
+            unsafe {
+                mapper.map_addr(virt, phys, self.pte_fields).unwrap();
+            }
         }
     }
 }
@@ -78,13 +109,17 @@ impl HypPageTable {
 
 /// A set of global mappings of the hypervisor that can be used to create page tables.
 pub struct HypMap {
-    mem_map: HwMemMap,
+    regions: ArrayVec<HypMapRegion, MAX_HYPMAP_REGIONS>,
 }
 
 impl HypMap {
     /// Create a new hypervisor map from a hardware memory mem map.
     pub fn new(mem_map: HwMemMap) -> HypMap {
-        HypMap { mem_map }
+        let regions = mem_map
+            .regions()
+            .filter_map(HypMapRegion::from_hw_mem_region)
+            .collect();
+        HypMap { regions }
     }
 
     /// Create a new page table based on this memory map.
@@ -100,8 +135,8 @@ impl HypMap {
             FirstStagePageTable::new(root_page).expect("creating first sv48");
 
         // Map all the regions in the memory map that the hypervisor could need.
-        for (base, size, perms) in self.mem_map.regions().filter_map(hyp_map_params) {
-            hyp_map_region(&sv48, base, size, perms, &mut || {
+        for r in &self.regions {
+            r.map(&sv48, &mut || {
                 hyp_mem.take_pages_for_hyp_state(1).into_iter().next()
             });
         }

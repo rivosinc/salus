d85b3f267aa37e131f551a7fd124886834d73209
Gianluca Guida <gianluca@rivosinc.com>
2022-12-09 16:56:10 +0000

    Introduce libuser and umode binary
    This patch introduces stub userspace binary and library for salus.
    
    Currently the code is compiled but unused.
    
    Original code by Dylan, with minor changes from me.
    
    Signed-off-by: Dylan Reid <dgreid@rivosinc.com>
    Signed-off-by: Gianluca Guida <gianluca@rivosinc.com>

---
+++ libuser/src/lib.rs
@@ -0,0 +1,21 @@
+// Copyright (c) 2022 by Rivos Inc.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#![no_std]
+
+use core::arch::{asm, global_asm};
+
+global_asm!(include_str!("task_start.S"));
+
+// Loop making ecalls as the kernel will kill the task on an ecall (the only syscall supported is
+// `exit`).
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    // Safe to make an ecall that won't return.
+    unsafe {
+        loop {
+            asm!("ecall");
+        }
+    }
+}

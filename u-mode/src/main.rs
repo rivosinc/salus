0a5938dd5c4c05903b8bbe34645cc17cf8d15882
Gianluca Guida <gianluca@rivosinc.com>
2022-12-20 20:12:44 +0000

    u-mode: create basic loop to serve requests from hypervisor.
    Signed-off-by: Gianluca Guida <gianluca@rivosinc.com>

--- u-mode/src/main.rs
+++ u-mode/src/main.rs
@@ -7,7 +7,35 @@
 
 extern crate libuser;
 
+use libuser::*;
+use u_mode_api::UmodeOp;
+
 #[no_mangle]
-extern "C" fn task_main(_data: u64) {
-    panic!("");
+extern "C" fn task_main(cpuid: u64) -> ! {
+    println!("umode/#{} initialized.", cpuid);
+    // Initialization done.
+    let mut res = Ok(());
+    loop {
+        // Return result and wait for next operation.
+        let req = hyp_nextop(res);
+        res = match req {
+            Ok(req) => match req.op() {
+                UmodeOp::Nop => Ok(()),
+                UmodeOp::Hello => {
+                    println!("----------------------------");
+                    println!(" ___________________");
+                    println!("< Hello from UMODE! >");
+                    println!(" -------------------");
+                    println!("        \\   ^__^");
+                    println!("         \\  (oo)\\_______");
+                    println!("            (__)\\       )\\/\\");
+                    println!("                ||----w |");
+                    println!("                ||     ||");
+                    println!("----------------------------");
+                    Ok(())
+                }
+            },
+            Err(err) => Err(err),
+        };
+    }
 }

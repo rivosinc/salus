d22d7efd7bdee622ce7e688bf1dfb7a5912b894a
Gianluca Guida <gianluca@rivosinc.com>
2023-01-23 11:16:09 +0000

    Update SBI main.
    Update to latest changes, nacl shmem fix and vendor patch (changes
    interface).
    
    Signed-off-by: Gianluca Guida <gianluca@rivosinc.com>

--- src/vm.rs
+++ src/vm.rs
@@ -724,6 +724,7 @@ impl<'a, T: GuestStagePagingMode> FinalizedVm<'a, T> {
                 self.handle_attestation_msg(attestation_func, active_vcpu.active_pages())
             }
             SbiMessage::Pmu(pmu_func) => self.handle_pmu_msg(pmu_func, active_vcpu).into(),
+            SbiMessage::Vendor(_) => EcallAction::Unhandled,
         }
     }
 

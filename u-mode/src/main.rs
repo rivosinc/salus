59a3e61626ff24bad2591d7817bbd534a5ead70b
Gianluca Guida <gianluca@rivosinc.com>
2023-01-25 16:53:31 +0000

    U-mode: Add memcopy op implementation
    Signed-off-by: Gianluca Guida <gianluca@rivosinc.com>

--- u-mode/src/main.rs
+++ u-mode/src/main.rs
@@ -8,7 +8,21 @@
 extern crate libuser;
 
 use libuser::*;
-use u_mode_api::UmodeOp;
+use u_mode_api::{Error as UmodeApiError, UmodeOp, UmodeRequest};
+
+fn op_memcopy(req: &UmodeRequest) -> Result<(), UmodeApiError> {
+    let in_addr = req.in_addr.ok_or(UmodeApiError::InvalidArgument)?;
+    // Safety: we trust the hypervisor to have mapped at `req.in_addr` `req.in_len` bytes for reading.
+    let input = unsafe { &*core::ptr::slice_from_raw_parts(in_addr as *const u8, req.in_len) };
+    let out_addr = req.out_addr.ok_or(UmodeApiError::InvalidArgument)?;
+    // Safety: we trust the hypervisor to have mapped at `req.out_addr` `req.out_len` bytes valid
+    // for reading and writing.
+    let output =
+        unsafe { &mut *core::ptr::slice_from_raw_parts_mut(out_addr as *mut u8, req.out_len) };
+    let len = core::cmp::min(input.len(), output.len());
+    output[0..len].copy_from_slice(&input[0..len]);
+    Ok(())
+}
 
 #[no_mangle]
 extern "C" fn task_main(cpuid: u64) -> ! {
@@ -34,6 +48,7 @@ extern "C" fn task_main(cpuid: u64) -> ! {
                     println!("----------------------------");
                     Ok(())
                 }
+                UmodeOp::MemCopy => op_memcopy(&req),
             },
             Err(err) => Err(err),
         };

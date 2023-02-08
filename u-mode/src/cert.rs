12a9abda7106ad98aa185ff663adf5a79d9e3323
Gianluca Guida <gianluca@rivosinc.com>
2023-02-08 12:57:24 +0000

    U-mode: Implement GetEvidence
    This commit implements the GetEvidence call, using rice to generate a
    certificate from the CSR passed from guest and the `GetEvidenceShared`
    obtained from the hypervisor.
    
    Signing of certificate is yet to be implemented.
    
    Signed-off-by: Gianluca Guida <gianluca@rivosinc.com>

---
+++ u-mode/src/cert.rs
@@ -0,0 +1,104 @@
+// Copyright (c) 2023 by Rivos Inc.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+extern crate libuser;
+use libuser::*;
+
+use der::Decode;
+use ed25519::Signature;
+use ed25519_dalek::Signer;
+use generic_array::GenericArray;
+use rice::x509::certificate::{Certificate, MAX_CERT_SIZE};
+use rice::x509::extensions::dice::tcbinfo::DiceTcbInfo;
+use rice::x509::request::CertReq;
+use rice::x509::MAX_CSR_LEN;
+use u_mode_api::cert::*;
+
+#[derive(Debug)]
+pub enum Error {
+    /// Input CSR buffer size too small.
+    CsrBufferTooSmall(usize, usize),
+    /// Cannot parse CSR.
+    CsrParseFailed(der::Error),
+    /// Cannot verify CSR.
+    CsrVerificationFailed(rice::Error),
+    /// Cannot add FWID extension.
+    FwidAddFailed(rice::Error),
+    /// Could not create TcbInfo Extensions.
+    TcbInfoFailed(rice::Error),
+    /// Cannot create Certificate.
+    CertificateCreationFailed(rice::Error),
+    /// Output Certificate buffer too small.
+    CertificateBufferTooSmall(usize, usize),
+}
+
+struct UmodeSigner {}
+
+impl Signer<Signature> for UmodeSigner {
+    fn try_sign(&self, _: &[u8]) -> Result<Signature, ed25519::Error> {
+        // TODO: Implement Signing of certificate.
+        Signature::from_bytes(&[0; 64])
+    }
+}
+
+pub fn get_certificate_sha384(
+    csr_input: &[u8],
+    evidence: GetEvidenceShared,
+    cert_output: &mut [u8],
+) -> Result<u64, Error> {
+    // Copy CSR from input.
+    let csr_len = csr_input.len();
+    if csr_len > MAX_CSR_LEN {
+        return Err(Error::CsrBufferTooSmall(csr_len, MAX_CSR_LEN));
+    }
+    let mut csr_bytes = [0u8; MAX_CSR_LEN];
+    csr_bytes[0..csr_len].copy_from_slice(csr_input);
+
+    let mut tcb_info_bytes = [0u8; 4096];
+    let mut tcb_info = DiceTcbInfo::new();
+    let hash_algorithm = const_oid::db::rfc5912::ID_SHA_384;
+
+    let csr = CertReq::from_der(&csr_bytes[0..csr_len]).map_err(Error::CsrParseFailed)?;
+
+    println!(
+        "U-mode CSR version {:?} Signature algorithm {:?}",
+        csr.info.version, csr.algorithm.oid
+    );
+
+    csr.verify().map_err(Error::CsrVerificationFailed)?;
+
+    for m in evidence.msmt_regs.iter() {
+        tcb_info
+            .add_fwid::<sha2::Sha384>(hash_algorithm, GenericArray::from_slice(m.as_slice()))
+            .map_err(Error::FwidAddFailed)?;
+    }
+
+    let tcb_info_extn = tcb_info
+        .to_extension(&mut tcb_info_bytes)
+        .map_err(Error::TcbInfoFailed)?;
+    let extensions: [&[u8]; 1] = [tcb_info_extn];
+
+    let mut cert_der_bytes = [0u8; MAX_CERT_SIZE];
+    let cert_der = Certificate::from_raw_parts(
+        evidence.cdi_id,
+        &evidence.cdi_id,
+        csr.info.subject.clone(),
+        csr.info.public_key,
+        Some(&extensions),
+        &UmodeSigner {},
+        &mut cert_der_bytes,
+    )
+    .map_err(Error::CertificateCreationFailed)?;
+
+    let cert_der_len = cert_der.len();
+    if cert_output.len() < cert_der_len {
+        return Err(Error::CertificateBufferTooSmall(
+            cert_output.len(),
+            cert_der_len,
+        ));
+    }
+    // Copy cert to output.
+    cert_output[0..cert_der_len].copy_from_slice(cert_der);
+    Ok(cert_der_len as u64)
+}
--- u-mode/src/main.rs
+++ u-mode/src/main.rs
@@ -21,11 +21,73 @@ use data_model::{VolatileMemory, VolatileSlice};
 use libuser::*;
 use u_mode_api::{Error as UmodeApiError, UmodeRequest};
 
+mod cert;
+
+// Dummy global allocator - panic if anything tries to do an allocation.
+struct GeneralGlobalAlloc;
+
+unsafe impl core::alloc::GlobalAlloc for GeneralGlobalAlloc {
+    unsafe fn alloc(&self, _layout: core::alloc::Layout) -> *mut u8 {
+        panic!("alloc called!");
+    }
+
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: core::alloc::Layout) {
+        panic!("dealloc called!");
+    }
+}
+
+// Global allocator linking (not usage) required by rice dependencies.
+#[global_allocator]
+static GENERAL_ALLOCATOR: GeneralGlobalAlloc = GeneralGlobalAlloc;
+
 struct UmodeTask {
     vslice: VolatileSlice<'static>,
 }
 
 impl UmodeTask {
+    // Get an attestation evidence.
+    // This function returns a serialized, DER formatted X.509 certificate.
+    // The attestation evidence is included as a certificate extension.
+    //
+    // Arguments:
+    //   csr_addr: starting address of the input Certificate Signing Request.
+    //   csr_len: size of the input Certificate Signing Request.
+    //   certout_addr: starting address of the output Certificate.
+    //   certout_len: size for the output Certificate.
+    //
+    // U-mode Shared Region: contains an instance of `GetEvidenceShared`.
+    fn op_get_evidence(
+        &self,
+        csr_addr: u64,
+        csr_len: usize,
+        certout_addr: u64,
+        certout_len: usize,
+    ) -> Result<u64, UmodeApiError> {
+        // Safety: we trust the hypervisor to have mapped at `csr_addr` `csr_len` bytes for reading.
+        let csr = unsafe { &*core::ptr::slice_from_raw_parts(csr_addr as *const u8, csr_len) };
+        // Safety: we trust the hypervisor to have mapped at `certout_addr` `certout_len` bytes valid
+        // for reading and writing.
+        let certout = unsafe {
+            &mut *core::ptr::slice_from_raw_parts_mut(certout_addr as *mut u8, certout_len)
+        };
+        let shared_data = self
+            .vslice
+            .get_ref(0)
+            .map_err(|_| UmodeApiError::Failed)?
+            .load();
+        cert::get_certificate_sha384(csr, shared_data, certout).map_err(|e| {
+            println!("get_certificate failed: {:?}", e);
+            use cert::Error::*;
+            match e {
+                CsrBufferTooSmall(_, _)
+                | CsrParseFailed(_)
+                | CsrVerificationFailed(_)
+                | CertificateBufferTooSmall(_, _) => UmodeApiError::InvalidArgument,
+                _ => UmodeApiError::Failed,
+            }
+        })
+    }
+
     // Run the main loop, receiving requests from the hypervisor and executing them.
     fn run_loop(&self) -> ! {
         let mut res = Ok(0);
@@ -35,6 +97,12 @@ impl UmodeTask {
             res = match req {
                 Ok(req) => match req {
                     UmodeRequest::Nop => Ok(0),
+                    UmodeRequest::GetEvidence {
+                        csr_addr,
+                        csr_len,
+                        certout_addr,
+                        certout_len,
+                    } => self.op_get_evidence(csr_addr, csr_len, certout_addr, certout_len),
                 },
                 Err(err) => Err(err),
             };

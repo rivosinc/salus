63790f77defc8f9bf4a392daa5af6c553db82e87
Gianluca Guida <gianluca@rivosinc.com>
2023-03-02 16:13:06 +0000

    libuser: Support CDI hypcalls
    Signed-off-by: Gianluca Guida <gianluca@rivosinc.com>

--- libuser/src/hypcalls.rs
+++ libuser/src/hypcalls.rs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use core::arch::asm;
-use u_mode_api::{Error as UmodeApiError, HypCall, TryIntoRegisters, UmodeRequest};
+use u_mode_api::{CdiOp, CdiSel, Error as UmodeApiError, HypCall, TryIntoRegisters, UmodeRequest};
 
 /// Send an ecall to the hypervisor.
 ///
@@ -66,4 +66,43 @@ pub fn hyp_nextop(result: Result<u64, UmodeApiError>) -> Result<UmodeRequest, Um
     }
     // In case there's an error on decoding the request, return it to caller.
     UmodeRequest::try_from_registers(&regs)
+}
+
+/// Get the ID of the CDI selected by `cdi_sel`.
+pub fn hyp_cdi_id(cdi_sel: CdiSel, id: &mut [u8]) {
+    let mut regs = [0u64; 8];
+    let hypc = HypCall::Cdi {
+        cdi_sel,
+        cdi_op: CdiOp::Id {
+            idout_addr: id.as_ptr() as u64,
+            idout_len: id.len() as u64,
+        },
+    };
+    hypc.to_registers(&mut regs);
+    // Safety: we trust the hypervisor to write at `idout_addr` for `idout_len` bytes. This range is
+    // entirely contained in `id`, of which we have a mutable reference.
+    unsafe {
+        ecall(&mut regs);
+    }
+}
+
+/// Sign a message with the secret key of the CDI selected by `cdi_sel`.
+pub fn hyp_cdi_sign(cdi_sel: CdiSel, msg: &[u8], signature: &mut [u8]) {
+    let mut regs = [0u64; 8];
+    let hypc = HypCall::Cdi {
+        cdi_sel,
+        cdi_op: CdiOp::Sign {
+            msg_addr: msg.as_ptr() as u64,
+            msg_len: msg.len() as u64,
+            signout_addr: signature.as_ptr() as u64,
+            signout_len: signature.len() as u64,
+        },
+    };
+    hypc.to_registers(&mut regs);
+    // Safety: we trust the hypervisor to write at `sign_addr` for `sign_len` bytes. This range is
+    // entirely contained in `signature`, of which we have a mutable reference. We also trust the
+    // hypervisor to read from `msg_addr` for `msg_len` bytes, which is entirely contained in `msg`.
+    unsafe {
+        ecall(&mut regs);
+    }
 }

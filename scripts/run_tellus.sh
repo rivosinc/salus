b07959130dbd19764a90dd8c021de87546746c30
Dylan Reid <dgreid@rivosinc.com>
2025-05-05 17:55:00 -0700

    scripts: update iommu args
    Now that iommu is ratified, use the new command line options.
    
    Signed-off-by: Dylan Reid <dgreid@rivosinc.com>

--- scripts/run_tellus.sh
+++ scripts/run_tellus.sh
@@ -9,4 +9,5 @@ ${QEMU_BIN} \
     ${MACH_ARGS} \
     -kernel ${SALUS_BINS}salus \
     -device guest-loader,kernel=${TELLUS_BINS}tellus_guestvm,addr=${KERNEL_ADDR} \
+    ${IOMMU_ARGS} \
     ${EXTRA_QEMU_ARGS}
